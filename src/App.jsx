d730ad2f4ea9c96aa2e59b7d7df12977ac6ddc60
TejashwarReddy142 <tejashwarreddy.vuyyuru@slu.edu>
2025-02-25 15:54:26 +0000

    updated style sheet with all instructions from lab

--- src/App.jsx
+++ src/App.jsx
@@ -7,5 +7,31 @@ function App() {
     </div>
   );
 }
+import Header from './components/Header';
 
+import productData from './data/full-products';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 export default App;
+// App.jsx
+import { Route, Routes} from 'react-router-dom';
+
+// ...
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      
+        <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
+      
+    </div>
+  );
+}
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,3 +7,13 @@ export default function Button({text, handleClick}) {
     </a>
   )
 }
+return (
+  <div className="cf pa2">
+
+    <div className="mt2 mb2">
+      // Using the data prop, we map over the list of products and render a Card component for each product
+      {data.map((product) => (
+        <Card key={product.id} {...product} />
+      ))}
+    </div>
+    
